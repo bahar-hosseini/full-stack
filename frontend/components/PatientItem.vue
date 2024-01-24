e19144316a7d085a7cfe3978ca4b62cfc7cdcb23
Bahar Hosseini <bahar.hosseini@gmail.com>
2024-01-24 00:17:51 -0500

    add patientItem card to break down patient component

---
+++ frontend/components/PatientItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <li
+    class="flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50"
+  >
+    <div>
+      <NuxtLink
+        :to="`/patient/${patient.id}`"
+        class="font-bold block text-gray-600"
+      >
+        {{ patient.firstname }} {{ patient.lastname }}
+      </NuxtLink>
+      <span class="text-gray-500 text-sm">
+        {{ patient.display_name }}
+      </span>
+      <span class="text-gray-500 text-sm"> </span>
+    </div>
+
+    <div class="text-gray-600 text-lg">
+      <Icon name="teenyicons:user-solid" color="sky" />
+    </div>
+  </li>
+</template>
+
+<script setup>
+const props = defineProps(['patient']);
+</script>
+
--- frontend/pages/patient/index.vue
+++ frontend/pages/patient/index.vue
@@ -1,23 +1,17 @@
 <template>
   <div>
-    <div v-if="pending">Loading...</div>
-    <div v-else>
-      <div v-if="error">{{ error }}</div>
-      <div v-else>
-        <ul>
-          <li v-for="patient in patients" :key="patient.id">
-            <NuxtLink :to="'/patient/' + patient.id">
-              {{ patient.firstname }} {{ patient.lastname }}
-            </NuxtLink>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <h1>Test</h1>
+    <ul>
+      <PatientItem
+        v-for="patient in patients"
+        :key="patient.id"
+        :patient="patient"
+      />
+    </ul>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
 import { useFetchPatients } from '@/api/useFetchPatients';
 
 const { patients, pending, error, fetchData } = useFetchPatients();
